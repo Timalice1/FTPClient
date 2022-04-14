e5479cada661a1502beaba3fd0c0f6c541604006
Timalice1 <newacccc2@gmail.com>
2022-04-14 18:16:22 +0300

    init

---
+++ FTPClient/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace FTPClient {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.host = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.usersList = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(42, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // host
+            // 
+            this.host.Location = new System.Drawing.Point(93, 5);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(100, 20);
+            this.host.TabIndex = 3;
+            this.host.Text = "ftp://";
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(93, 30);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 4;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(93, 56);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 5;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 89);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 89);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save profile";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // usersList
+            // 
+            this.usersList.FormattingEnabled = true;
+            this.usersList.Location = new System.Drawing.Point(12, 118);
+            this.usersList.Name = "usersList";
+            this.usersList.Size = new System.Drawing.Size(181, 134);
+            this.usersList.TabIndex = 8;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(61, 258);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(85, 23);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 291);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.usersList);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.host);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FTPClient";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox host;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox usersList;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ FTPClient/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FTPClient {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, System.EventArgs e) {
+
+        }
+
+        class User {
+            public string Host { get; set; }
+            public string Login { get; set; }
+            public string Password { get; set; }
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e) {
+            try {
+                CheckData();
+                var user = new User() {
+                    Host = host.Text,
+                    Login = login.Text,
+                    Password = password.Text
+                };
+
+                //Serialize user class to Json file
+                string filePath = $@"../../UserData/{user.Login}.json";
+                string data = JsonSerializer.Serialize(user, new JsonSerializerOptions() { WriteIndented = true});
+                File.WriteAllText(filePath, data);
+
+                //Add saved user to listView
+                foreach(var item in usersList.Items) {
+                    if (item.Equals(user.Login))
+                        throw new Exception("This profile already exist");
+                }
+                usersList.Items.Add(user.Login);
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, System.EventArgs e) {
+            try {
+                if (usersList.SelectedItem == null)
+                    throw new Exception("User not selected");
+
+                string path = $@"../../UserData/{usersList.SelectedItem}.json";
+                string data = File.ReadAllText(path);
+                var user = JsonSerializer.Deserialize<User>(data);
+
+                host.Text = user.Host;
+                login.Text = user.Login;
+                password.Text = user.Password;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            var files = Directory.GetFiles(@"../../UserData");
+            foreach(var file in files) {
+                string name = file.Remove(0, file.LastIndexOf("\\") + 1);
+                usersList.Items.Add(name.Remove(name.LastIndexOf(".")));
+            }
+        }
+
+        private void CheckData() {
+            //Check input data
+            var regex = new Regex(@"ftp://\d{3}.\d{3}.\d+.\d+");
+            if (!regex.IsMatch(host.Text))
+                throw new Exception("Host not valid");
+            if (login.Text == "")
+                throw new Exception("Login cannot be empty");
+            if (password.Text == "")
+                throw new Exception("Password cannot be empty");
+        }
+    }
+}
